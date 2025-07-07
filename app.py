fd88e1d66a90a041424c9bb0634689c438d1264e
Madhan12-tech <madhancg.18@gmail.com>
2025-07-07 14:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 app.secret_key = 'secretkey'
 
 # --- DB Connection ---
-def get_db():
-    conn = sqlite3.connect("database.db")
-    conn.row_factory = sqlite3.Row
-    return conn
+
 
 # --- Initialize DB with admin user ---
 def init_db():
