3d6f8c0635789d6164c29825ac4eea7668866d22
Madhan12-tech <madhancg.18@gmail.com>
2025-07-08 15:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = 'secretkey'
 
 # ---------- ✅ Database Connection ----------
 def get_db():
-    conn = get_db()
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
